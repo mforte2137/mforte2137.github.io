5a4caf7901dc3e461074a5ce005fa60458784c13
mforte2137 <mike@mmforte.net>
2025-04-18 15:24:29 -0400

    Update get-website-images.js

--- netlify/functions/get-website-images.js
+++ netlify/functions/get-website-images.js
@@ -7,8 +7,14 @@ exports.handler = async function(event, context) {
     const { url } = JSON.parse(event.body);
     
     // Fetch the webpage
-    const response = await axios.get(url);
-    const html = response.data;
+   const response = await axios.get(url, {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://www.google.com/'
+  }
+});
     
     // Extract images
     const $ = cheerio.load(html);
