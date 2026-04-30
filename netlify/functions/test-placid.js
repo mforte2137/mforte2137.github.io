9208c4af41aa2a93a215112231c6a02f61bc7e0c
mforte2137 <mike@mmforte.net>
2026-04-30 17:25:47 -0400

    Update test-placid.js

--- netlify/functions/test-placid.js
+++ netlify/functions/test-placid.js
@@ -40,9 +40,10 @@ exports.handler = async (event) => {
       body: JSON.stringify({
         template_uuid: PLACID_TEMPLATE,
         layers: {
-          photo:       { image:      TEST_PHOTO  },
-          brand_color: { background: TEST_COLOR  },
-          logo:        { image:      TEST_LOGO   }
+          photo:      { image:      TEST_PHOTO },
+          chevron:    { background: TEST_COLOR },
+          accent_bar: { background: TEST_COLOR },
+          logo:       { image:      TEST_LOGO  }
         }
       })
     });
